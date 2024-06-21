f430b015864ae726c8f43549b3b2b8eef5a8124e
Sameem <sameemikram89@gmail.com>
2024-06-22 00:53:42 +0500

    Added authentication for signup and login

--- kinect/settings.py
+++ kinect/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # custom app
-    'kinect_core'
+    'kinect_core',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- kinect_core/views.py
+++ kinect_core/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
+
 
 # Create your views here.
 
-def login(request):
-    return render(request, 'kinect_core/login.html')
-def signup(request):
-    return render(request, 'kinect_core/signup.html')
+def login_user(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect()
+        else:
+            messages.success(request, "There was an error logging In, Try again...")
+            return redirect("login")
+    else:
+        return render(request, 'kinect_core/login.html')
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created successfully")
+            return redirect('login')
+    else:
+        form = SignUpForm()
+    return render(request, 'kinect_core/signup.html',{
+        'form': form
+    })
 
 
 
