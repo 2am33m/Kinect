e3e377cf97823d33eeed708a9a42379b25bd2761
Sameem <sameemikram89@gmail.com>
2024-06-25 16:56:49 +0500

    Added a functioning authentication system

--- kinect_core/forms.py
+++ kinect_core/forms.py
@@ -7,4 +7,10 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('first_name', 'last_name', 'username','email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email address is already in use.")
+        return email
--- kinect_core/views.py
+++ kinect_core/views.py
@@ -8,9 +8,9 @@ from .forms import SignUpForm
 
 def login_user(request):
     if request.method == 'POST':
-        email = request.POST['email']
+        username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username=email, password=password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect()
@@ -19,18 +19,23 @@ def login_user(request):
             return redirect("login")
     else:
         return render(request, 'kinect_core/login.html')
+
+
 def signup_view(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=email, password=password)
             messages.success(request, "Account created successfully")
             return redirect('login')
     else:
         form = SignUpForm()
-    return render(request, 'kinect_core/signup.html',{
+    return render(request, 'kinect_core/signup.html', {
         'form': form
     })
 
 
-
+7
